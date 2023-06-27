422052b36667c9469af71204983559266f9d72f3
Sebastian Coral <sebasttiancorall7@gmail.com>
2023-06-27 18:31:01 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+function showalert() { alert('Hola mundo'); }
+
+
+//function updateName(){
+	//let nombre = prompt("Cambia tu nombre");
+	//if(nombre == '') {
+	//	nombre = "usuario Desconocido";
+	//}
+	//document.getElementById("usuario").innerHTML = `Bienvenido:${nombre}`;
+//}
+
+//updateName();
